393759fb34631fbb341c170ad8c3f465d0597ee5
Adam Kariv <adam.kariv@gmail.com>
2023-11-13 11:15:49 +0200

    build

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,3 @@
-from .auth import setup_auth
-from .search import setup_search
-from .query import setup_query
+from .auth import setup_auth  # noqa: W0611
+from .search import setup_search  # noqa: W0611
+from .query import setup_query  # noqa: W0611
