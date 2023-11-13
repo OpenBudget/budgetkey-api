3baa557affc8bfac620fa3ad65e90e63a2a35c11
Adam Kariv <adam.kariv@gmail.com>
2023-11-13 13:39:41 +0200

    lint

--- modules/auth.py
+++ modules/auth.py
@@ -5,6 +5,7 @@ from dgp_oauth2 import make_blueprint as auth_blueprint
 
 def setup_auth(app):
     app.register_blueprint(
-        auth_blueprint(os.environ.get('EXTERNAL_ADDRESS'), db_connection_string=os.environ['DATABASE_PRIVATE_URL']),
+        auth_blueprint(os.environ.get('EXTERNAL_ADDRESS'),
+                       db_connection_string=os.environ['DATABASE_PRIVATE_URL']),
         url_prefix='/api/auth/'
     )
