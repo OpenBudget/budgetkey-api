ddc61f4e1348155b6fa71412e871d5f529352849
Adam Kariv <adam.kariv@gmail.com>
2023-11-18 16:19:54 +0200

    Debug auth

--- server.py
+++ server.py
@@ -10,10 +10,11 @@ else:
     import logging
     import sys
     gunicorn_error_logger = logging.getLogger('gunicorn.error')
+
     app.logger.handlers.extend(gunicorn_error_logger.handlers)
-    dgp_auth_logger = logging.getLogger('dgp-oauth2')
-    dgp_auth.logger.handlers.extend(logging.StreamHandler(stream=sys.stdout))
-    dgp_auth_logger.setLevel(logging.DEBUG)
-    app.logger.handlers.extend(dgp_auth_logger.handlers)
     app.logger.setLevel(logging.DEBUG)
     app.logger.info('SERVER STARTING')
+
+    dgp_auth_logger = logging.getLogger('dgp-oauth2')
+    dgp_auth_logger.setLevel(logging.DEBUG)
+    dgp_auth_logger.handlers.extend(gunicorn_error_logger.handlers)
