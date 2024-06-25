17748f4bb0c0f7abfebbac76ea19daa8d844f3cb
Adam Kariv <adam.kariv@gmail.com>
2024-06-25 19:53:16 +0300

    bump apisql

--- budgetkey_api/modules/query.py
+++ budgetkey_api/modules/query.py
@@ -5,10 +5,11 @@ from .caching import add_cache_header
 
 
 MAX_ROWS = int(os.environ.get('MAX_ROWS', 1000))
-
+EXTERNAL_ADDRESS = os.environ.get('EXTERNAL_ADDRESS')
 
 def setup_query(app, cache):
     bp = apisql_blueprint(connection_string=os.environ['DATABASE_READONLY_URL'],
-                          max_rows=MAX_ROWS, debug=False, cache=cache)
+                          max_rows=MAX_ROWS, debug=False, cache=cache,
+                          external_url=f'https://{EXTERNAL_ADDRESS}/api')
     add_cache_header(bp, 3600)
     app.register_blueprint(bp, url_prefix='/api/')
